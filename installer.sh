2f8b7d531d997a8b078ed8c1c9ea60185a2f6517
user-firepluto <eduardovianapires987@gmail.com>
2026-02-19 14:18:37 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chmod +x calc
 chmod +x calc.py
-mkdir /usr/local/bin/calc-files
+sudo mkdir /usr/local/bin/calc-files
 sudo cp calc.py /usr/local/bin/calc-files
 sudo cp calc /usr/local/bin
