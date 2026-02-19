52cc9c4e79036e50dfc1be91f9383d210d2e78d1
user-firepluto <eduardovianapires987@gmail.com>
2026-02-19 13:58:03 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 chmod +x calc
 chmod +x calc.py
-chmod +x remove.sh
-mkdir /home/$USER/.miniprojects/calc
-sudo cp calc.py /home/$USER/.miniprojects/calc
-sudo cp remove.sh /home/$USER/.miniprojects/calc
+mkdir /usr/local/bin/calc
+sudo cp calc.py /usr/local/bin/calc
 sudo cp calc /usr/local/bin
 echo "Instalation completed"
 
